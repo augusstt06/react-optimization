22feb12c3de2f68ebab520c9fd05c84a07609374
augusstt06 <augusstt06@gmail.com>
2024-04-19 23:57:45 +0900

    test optimization

--- src/pages/fetch/childs/variableFetch/VariableFetch.tsx
+++ src/pages/fetch/childs/variableFetch/VariableFetch.tsx
@@ -4,6 +4,17 @@ import MemorizeInput from '@/components/CustomInput.tsx';
 import MemorizeMapping from '@/pages/fetch/childs/variableFetch/childs/Mapping.tsx';
 import { Ttodo } from '@/pages/fetch/types';
 
+const fetchData = async () => {
+  try {
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+    const data = (await res.json()).slice(0, 5);
+    console.log(data);
+    return data;
+    // setTodos(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
 const VariableFetch = () => {
   console.log('VariableFetch Component is render!');
   const [isView, setIsView] = useState<boolean>(false);
@@ -14,17 +25,9 @@ const VariableFetch = () => {
   useEffect(() => {
     if (!prevIsViewRef.current && isView) {
       console.log('Data Fetching...');
-      const fetchData = async () => {
-        try {
-          const res = await fetch('https://jsonplaceholder.typicode.com/todos');
-          const data = (await res.json()).slice(0, 5);
-          console.log(data);
-          setTodos(data);
-        } catch (err) {
-          console.log(err);
-        }
-      };
-      void fetchData();
+
+      // void fetchData();
+      fetchData().then((data) => setTodos(data));
     }
     prevIsViewRef.current = isView;
   }, [isView]);
